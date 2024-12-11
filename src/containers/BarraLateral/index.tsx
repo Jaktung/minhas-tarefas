b14a3280ee21c6f59d26012e02289c4606133390
luiz <luiz@exemplo.com>
2024-12-11 16:45:24 -0300

    parametrizando os filtros

--- src/containers/BarraLateral/index.tsx
+++ src/containers/BarraLateral/index.tsx
@@ -1,8 +1,21 @@
+import FiltroCard from '../../components/FiltroCard'
+import * as S from './styles'
+
 const BarraLateral = () => {
   return (
-    <aside>
-      <p>Barra Lateral</p>
-    </aside>
+    <S.Aside>
+      <div>
+        <S.Campo type="text" placeholder="Buscar" />
+        <S.Filtros>
+          <FiltroCard legenda="pendentes" contador={3} />
+          <FiltroCard legenda="concluidas" contador={4} />
+          <FiltroCard legenda="urgentes" contador={2} />
+          <FiltroCard legenda="importantes" contador={2} />
+          <FiltroCard legenda="normal" contador={3} />
+          <FiltroCard ativo legenda="todas" contador={7} />
+        </S.Filtros>
+      </div>
+    </S.Aside>
   )
 }
 
