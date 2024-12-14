5fb1705a1d735b91d2dd709f41f35961bf43fb46
luiz <luiz@exemplo.com>
2024-12-14 19:05:15 -0300

    componentização do projeto

--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -1,8 +1,44 @@
+import Tarefa from '../../components/Tarefas'
+import { Container } from './styles'
+
+const tarefas = [
+  {
+    titulo: 'Estudar Typescript',
+    descricao: 'Ver a aula da EBAC',
+    prioridade: 'importante',
+    status: 'pendente'
+  },
+  {
+    titulo: 'Pagar a conta de internet',
+    descricao: 'Baixar fatura no gmail',
+    prioridade: 'urgente',
+    status: 'concluída'
+  },
+  {
+    titulo: 'Ir para a academia',
+    descricao: 'Fazer treino B',
+    prioridade: 'importante',
+    status: 'pendente'
+  }
+]
+
 const ListaDeTarefas = () => {
   return (
-    <main>
+    <Container>
       <p>2 tarefas marcadas como: ”todas” e ”termo”</p>
-    </main>
+      <ul>
+        {tarefas.map((t) => (
+          <li key={t.titulo}>
+            <Tarefa
+              titulo={t.titulo}
+              prioridade={t.prioridade}
+              status={t.status}
+              descricao={t.descricao}
+            />
+          </li>
+        ))}
+      </ul>
+    </Container>
   )
 }
 
