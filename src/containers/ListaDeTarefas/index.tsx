eb367aadd17d02f631b065c6a4c8cb7fd4e7fa13
luiz <luiz@exemplo.com>
2024-12-16 16:48:53 -0300

    simplificando com react-redux

--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -1,28 +1,12 @@
+import { useSelector } from 'react-redux'
+
 import Tarefa from '../../components/Tarefas'
 import { Container } from './styles'
 
-const tarefas = [
-  {
-    titulo: 'Estudar Typescript',
-    descricao: 'Ver a aula da EBAC',
-    prioridade: 'importante',
-    status: 'pendente'
-  },
-  {
-    titulo: 'Pagar a conta de internet',
-    descricao: 'Baixar fatura no gmail',
-    prioridade: 'urgente',
-    status: 'concluída'
-  },
-  {
-    titulo: 'Ir para a academia',
-    descricao: 'Fazer treino B',
-    prioridade: 'importante',
-    status: 'pendente'
-  }
-]
+import { RootReducer } from '../../store'
 
 const ListaDeTarefas = () => {
+  const { tarefas } = useSelector((state: RootReducer) => state)
   return (
     <Container>
       <p>2 tarefas marcadas como: ”todas” e ”termo”</p>
